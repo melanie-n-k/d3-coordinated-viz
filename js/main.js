918f18efd6721aea645b30e5fd43d340e446c6fc
melanie-n-k <45474048+melanie-n-k@users.noreply.github.com>
2019-04-01 12:02:07 -0500

    load data
    use Promise to load all data in parallel (conduct multiple AJAX calls)

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 /* javascript sheet mkohls */
 //D3 lab
+
+//when window loads, start running script
+window.onload = setMap();
+
+//set up choropleth with setMap function
+function setMap(){
+  //use Promise to make the data load in parallel
+  var promises = [];
+  promises.push(d3.csv("data/state_data.csv")); //load csv with data attributes for each state
+  promises.push(d3.json("data/contiguous.topojson")); //load background spatial data
+  promises.push(d3.json("data/forest-states.topojson")); //load choropleth spatial data
+  Promise.all(promises).then(callback);
+};
