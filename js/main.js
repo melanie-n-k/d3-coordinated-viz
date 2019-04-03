f4893cf1147c2bcce20983a97cc38af43cf7f02c
melanie-n-k <45474048+melanie-n-k@users.noreply.github.com>
2019-04-03 15:14:21 -0500

    join
    joined csv data with enumeration units using adm1_code as primary key

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ function setMap(){
       .attr("width", width)
       .attr("height", height);
 
-  //set up porjection: Albers equal area for usa
+  //set up projection: Albers equal area for usa
   var projection = d3.geoAlbers()
       .center([0, 38.5])
       .rotate([98, 0, 0])
@@ -63,11 +63,38 @@ function setMap(){
     //console.log(forestData);
     //console.log(contig);
     //console.log(forests);
+
  var contigUS = topojson.feature(contig, contig.objects.contiguous_US),
      forestStates = topojson.feature(forests, forests.objects.FIA_states).features;
 
+//variables for data join
+ var attrArray = ["forest_land", "timber_land", "live_trees", "dead_trees", "carbon_live"];
+
+//loop through csv to assign each set of csv attribute values to geojson region
+ for (var i=0; i<forestData.length; i++){
+    var csvState = forestData[i]; //the current state
+    var csvKey = csvState.adm1_code; //the CSV primary key
+
+    //loop through geojson states to find correct state
+    for (var a=0; a<forestStates.length; a++){
+
+        var geojsonProps = forestStates[a].properties; //the current state geojson properties
+        var geojsonKey = geojsonProps.adm1_code; //the geojson primary key
+
+        //where primary keys match, transfer csv data to geojson properties object
+        if (geojsonKey == csvKey){
+
+            //assign all attributes and values
+            attrArray.forEach(function(attr){
+                var val = parseFloat(csvState[attr]); //get csv attribute value
+                geojsonProps[attr] = val; //assign attribute and value to geojson properties
+            });
+        };
+    };
+};
         //examine the results
-        console.log(contigUS);
+        console.log(forestData);
+        console.log(attrArray);
         console.log(forestStates);
 
   var states = map.append("path")
